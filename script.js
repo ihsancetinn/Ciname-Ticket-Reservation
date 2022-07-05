cfe704a47822d10d41b441b957fdf01972406fd3
ihsancetinn <ihsancetin92@gmail.com>
2022-07-05 12:42:13 +0300

    amount and price

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 const container = document.querySelector('.container');
+const count = document.querySelector('#count');
+const amount = document.querySelector('#amount');
+const select = document.querySelector('#movie');
+const seats = document.querySelectorAll('.seat:not(.reserved');
 
-container.addEventListener('click', function(e) {
-    if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')){
+
+
+container.addEventListener('click', function (e) {
+    if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
         e.target.classList.toggle('selected');
-
-let selectedSeatCount = container.querySelectorAll('.seat.selected').length;
-console.log(selectedSeatCount);
-
-
-
-
-
-
+        calculateTotal();
 
 
     }
-})
+});
+
+select.addEventListener('change', function (e) {
+    calculateTotal();
+});
+
+function calculateTotal() {
+    const selectedSeats = container.querySelectorAll('.seat.selected');
+
+    const selectedSeatsArr = [];
+    const seatsArr = [];
+
+    selectedSeats.forEach(function (seat) {
+        selectedSeatsArr.push(seat);
+    });
+
+    seats.forEach(function (seat) {
+        seatsArr.push(seat);
+    });
+
+    let selectedSeatIndexs = selectedSeatsArr.map(function (seat){
+        return seatsArr.indexOf(seat)  ;
+    });
+    console.log(selectedSeatIndexs);
+
+    let selectedSeatCount = selectedSeats.length;
+    count.innerText = selectedSeatCount;
+    amount.innerText = parseFloat(select.value) * selectedSeatCount;
+
+saveToLocalStorage(selectedSeatIndexs);
+}
+function saveToLocalStorage(indexs){
+    localStorage.setItem('selectedSeats', JSON.stringify(indexs));
+}
