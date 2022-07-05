c8eb9c007fd3476771fe6d24a0fbeada23a8c06c
ihsancetinn <ihsancetin92@gmail.com>
2022-07-05 17:05:21 +0300

    Localstorage

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const select = document.querySelector('#movie');
 const seats = document.querySelectorAll('.seat:not(.reserved');
 
 
+getFromLocalStorage();
+calculateTotal();
+
+
 
 container.addEventListener('click', function (e) {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
@@ -44,6 +48,32 @@ function calculateTotal() {
 
 saveToLocalStorage(selectedSeatIndexs);
 }
+
+function getFromLocalStorage (){
+const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+
+if(selectedSeats != null && selectedSeats.length >0){
+    seats.forEach(function(seat, index) {
+        if(selectedSeats.indexOf(index) > -1){
+            seat.classList.add('selected');
+        }
+
+    })
+}
+
+
+
+if(selectedMovieIndex != null){
+    select.selectedIndex = selectedMovieIndex;
+}
+
+
+}
+
+
 function saveToLocalStorage(indexs){
     localStorage.setItem('selectedSeats', JSON.stringify(indexs));
+    localStorage.setItem('selectedMovieIndex', select.selectedIndex);
 }
